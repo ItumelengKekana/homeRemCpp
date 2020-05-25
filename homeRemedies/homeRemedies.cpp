6c6be7becbe47e33dd3420f9eabf9b97e5ecb950
Itumeleng Kekana <kekanaitumeleng209@gmail.com>
2020-05-25 21:02:56 +0200

    relocated functions and added reference to username

--- homeRemedies/homeRemedies.cpp
+++ homeRemedies/homeRemedies.cpp
@@ -11,23 +11,17 @@ using namespace std;
 
 class User
 {
-	protected:
-		string name, email, password;
-		string products[4];
+	private:
+		string name, email, password, remedy;
 
 	public: 
 		User();
 		void setValues(string, string, string);
+		void setRem(string);
 		string getName() { return name; };
 		string getPass() { return password; };
 		string getEmail() { return email; };
-		void login();
-		void getProd(string, int);
-		void logHistory(int);
-		string search(int);
-		int menu();
-		void display();
-
+		string getRem() { return remedy; };
 };
 
 User::User()
@@ -35,18 +29,23 @@ User::User()
 	name = " ";
 	email = " ";
 	password = " ";
+	remedy = " ";
 }
 
 void User::setValues(string nm, string em, string pass)
 {
-	nm = name;
-	em = email;
-	pass = password;
+	name = nm;
+	email = em;
+	password = pass;
 }
 
-void User::login()
+void User::setRem(string rm)
+{
+	remedy = rm;
+}
+
+void login(User& usr)
 {	
-	User usr;
 	string nm, em, pass;
 
 	cout << "Enter your username: ";
@@ -57,70 +56,13 @@ void User::login()
 	cin >> pass;
 
 	usr.setValues(nm, em, pass);
-
-	cout << "Welcome" << usr.getName() << endl;
 }
 
-void User::getProd(string fname, int val)
-{
-	ifstream inFile(fname.c_str());
-	string line;
-
-	if (!inFile)
-	{
-		cout << "File could not open. Please check if it exists";
-	}
-	else
-	{
-		while (getline(inFile, line))
-		{
-			for (int x = 0; x < 4; x++)
-			{
-				products[val] = line;
-			}
-		}
-
-		for (int x = 0; x < 4; x++)
-		{
-			cout << products[val];
-		}
-	}
-}
-
-string User::search(int num)
-{
-	string name;
-
-	switch (num)
-	{
-	case 1: 
-		name = "Cold.txt";
-		break;
-	case 2: 
-		name = "headache.txt";
-		break;
-	case 3:
-		name = "back.txt";
-		break;
-	case 4:
-		name = "throat.txt";
-		break;
-	case 5: 
-		break;
-	default:
-		cout << "Please enter a valid option";
-		break;
-	}
-
-	return name;
-}
-
-int User::menu()
+int menu(User usr)
 {
 	int num;
-	User usr;
 
-
+	cout << "Welcome " << usr.getName() << endl;
 	cout << "\nHome Remedy Menu: " << endl
 		<< "====================================" << endl
 		<< "What ails you at this moment?\n" << endl
@@ -134,9 +76,57 @@ int User::menu()
 	return num;
 }
 
-void User::logHistory(int op)
+string search(int num, User usr)
 {
-	User usr;
+	string name;
+
+	switch (num)
+	{
+	case 1:
+		name = "Cold.txt";
+		break;
+	case 2:
+		name = "headache.txt";
+		break;
+	case 3:
+		name = "back.txt";
+		break;
+	case 4:
+		name = "throat.txt";
+		break;
+	case 5:
+		break;
+	default:
+		cout << "Please enter a valid option";
+		break;
+	}
+
+	usr.setRem(name);
+	return name;
+}
+
+void getProd(string fname)
+{
+	ifstream inFile(fname.c_str());
+	string line, product;
+
+	if (!inFile)
+	{
+		cout << "File could not open. Please check if it exists";
+	}
+	else
+	{
+		while (getline(inFile, line))
+		{
+			product = line;
+		}
+
+		cout << product;
+	}
+}
+
+void logHistory(User usr)
+{
 	string fname = "log.txt";
 	ofstream outFile;
 
@@ -153,24 +143,22 @@ void User::logHistory(int op)
 	}
 	else
 	{
-		outFile << usr.getName() << " Searched for " << usr.search(op) << " on " << /*asctime(ti)*/ endl;
+		outFile << usr.getName() << " Searched for " << usr.getRem() << " on " << /*asctime(ti)*/ endl;
 		outFile.close();
-		cout << "Session logged!" << endl;
+		cout << "\n\nSession logged!" << endl;
 	}
 }
 
-void User::display()
+void display(User usr)
 {
-	User usr;
 	int option;
 	string file;
 
-	usr.login();
-	option = usr.menu();
-	file = usr.search(option);
-	usr.getProd(file, option);
-	usr.logHistory(option);
-
+	login(usr);
+	option = menu(usr);
+	file = search(option, usr);
+	getProd(file);
+	logHistory(usr);
 }
 
 
@@ -178,7 +166,7 @@ int main()
 {
 	User person;
 	
-	person.display();
+	display(person);
 
 	system("Pause");
 	return 0;
